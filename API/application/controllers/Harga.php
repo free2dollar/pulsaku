90bb93f88db933bf11347f5d382d237cf7f50cb9
refanvolis <seallover321@gmail.com>
2017-12-29 14:03:37 +0700

    penambahan post page harga

--- API/application/controllers/Harga.php
+++ API/application/controllers/Harga.php
@@ -11,7 +11,7 @@ class Harga extends REST_Controller {
         $this->load->database();
     }
 
-    //Menampilkan data kontak
+    //Menampilkan data harga
     function index_get() {
         $kode = $this->get('kode');
         if ($kode == '') {
@@ -21,6 +21,21 @@ class Harga extends REST_Controller {
             $harga = $this->db->get('Harga')->result();
         }
         $this->response($harga, 200);
+    }
+
+    function index_post() {
+        $data = array(
+                    'kode'           => $this->post('kode'),
+                    'id_operator'    => $this->post('id_operator'),
+                    'nominal'        => $this->post('nominal'),
+                    'harga'          => $this->post('harga'),
+                    'status'         => $this->post('status'));
+        $insert = $this->db->insert('Harga', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
     }
 
 
